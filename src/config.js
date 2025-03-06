8e5030229289d8bdf89baf241fcfa3656559a08c
skworkflow412 <skworkflow01@gmail.com>
2025-03-06 19:42:03 +0530

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,7 @@
+// src/config.js
+module.exports = {
+    TELEGRAM_API_ID: process.env.TELEGRAM_API_ID,
+    TELEGRAM_API_HASH: process.env.TELEGRAM_API_HASH,
+    TELEGRAM_PHONE: process.env.TELEGRAM_PHONE,
+    TELEGRAM_SESSION: "", // Empty session string for initial login
+};
